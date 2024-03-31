3d82cbf8534b06c6d0d702a41094ce9d1955a087
Juancasta14 <juanfelipe9902@gmail.com>
2024-03-31 12:51:48 -0500

    last update

--- js/calcular.js
+++ js/calcular.js
@@ -1,3 +1,27 @@
+document.getElementById("formularioF").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var numNumebers = document.getElementById("numeroF").value;
+    var numeroF=0;
+    var resultadoF=document.getElementById("resultadoF");
+    if(numNumebers <=0){
+        resultadoF.innerHTML = "Ingrese números mayores a 0";
+
+    }else{
+        for(var i =0; i <=numNumebers; i++){
+            numeroF = numeroF+i;
+        }
+    resultadoF.innerHTML = "Numero sucesión: " + numNumebers + "<br>"+
+                            "Suma de los números: " + numeroF;  
+    }
+                  
+});
+
+document.getElementById("limpiarF").addEventListener("click", function() {
+    document.getElementById("formularioF").reset();
+    document.getElementById("resultadoF").innerHTML = "";
+    });
+
+
 document.getElementById("formulario").addEventListener("submit", function(event) {
     event.preventDefault();
     var nombre = document.getElementById("nombre").value;
@@ -18,3 +42,4 @@ document.getElementById("formulario").reset();
 // Limpiar el resultado mostrado
 document.getElementById("resultado").innerHTML = "";
 });
+
